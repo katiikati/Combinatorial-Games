efcb80512fb21fff1bad530e11e92c6ec1b9e2be
katiikati <166103545+katiikati@users.noreply.github.com>
2025-10-08 09:03:51 -0700

    Corrected update selection for any shape of the chocolate bar

--- chocolate_grid.gd
+++ chocolate_grid.gd
@@ -13,6 +13,9 @@ var last_selected: Node3D = null
 var current_row:int
 var current_col: int
 
+var prev_row: int
+var prev_col: int
+
 var deleted_points = []
 var existing_points = []
 var just_added: Vector2
@@ -20,10 +23,13 @@ var just_added: Vector2
 func _ready():
 	current_row = rows-1
 	current_col = cols-1
+	prev_row = current_row
+	prev_col = current_col
 	
 	var x_offset = (cols - 1) * distance * 0.5
 	var y_offset = (rows - 1) * distance * 0.5
 	
+	#spawn chocolate squares
 	for r in range(rows):
 		for c in range(cols):
 			var piece = piece_scene.instantiate()
@@ -43,33 +49,29 @@ func _unhandled_input(event: InputEvent):
 	if !start_game:
 		return
 	
+	#stuff when keys are pressed
 	if event is InputEventKey && event.pressed:
 		var moved = false
 		
 		if event.is_action_pressed("up"):
+			prev_row = current_row
 			current_row += 1
 			moved = true
 			
 		if event.is_action_pressed("down"):
+			prev_row = current_row
 			current_row -= 1
 			moved = true
 			
 		if event.is_action_pressed("right"):
+			prev_col = current_col
 			current_col += 1
 			moved = true
 			
 		if event.is_action_pressed("left"):
+			prev_col = current_col
 			current_col -= 1
 			moved = true
-			
-		if current_row < 0:
-			current_row = 0
-		if current_row > rows-1:
-			current_row = rows-1
-		if current_col < 0:
-			current_col = 0
-		if current_col > cols-1:
-			current_col = cols-1
 		
 		if moved:
 			update_selection()
@@ -78,6 +80,25 @@ func _unhandled_input(event: InputEvent):
 			eat_chunk()
 
 func update_selection():
+	#after WASD key pressed
+	print(existing_points)
+	if !Vector2(current_row, current_col) in existing_points:
+		print("nope")
+		if Vector2(prev_row, current_col) in existing_points:
+			current_row = prev_row
+		elif Vector2(current_row, prev_col) in existing_points:
+			current_col = prev_col
+	print("current", current_row, current_col)
+		
+	for child in get_children():
+		child.set_selected(false)
+		if child.row == current_row && child.col == current_col:
+			last_selected = child
+			
+	last_selected.set_selected(true)
+	
+func get_next_select():
+	#after chunk eaten, get new selected square
 	for child in get_children():
 		child.set_selected(false)
 		var child_point = Vector2(child.row, child.col)
@@ -85,6 +106,8 @@ func update_selection():
 			last_selected = child
 			
 	last_selected.set_selected(true)
+	current_row = last_selected.row
+	current_col = last_selected.col
 			
 func eat_chunk():
 	
@@ -109,11 +132,12 @@ func eat_chunk():
 		if current_col < 0:
 			current_col = 0
 		set_new_current = true
-		update_selection()
+		get_next_select()
 		
 	ui.next_play()
 	
 func eat_given_chunk(chunk: Vector2):
+	#eat a chunk given from best move in single play algorithm
 	just_added=Vector2(chunk.x, chunk.y)
 	for child in get_children():
 		if child.row >= chunk.x && child.col >= chunk.y:
@@ -127,4 +151,4 @@ func eat_given_chunk(chunk: Vector2):
 		current_row = 0
 	if current_col < 0:
 		current_col = 0
-	update_selection()
+	get_next_select()
